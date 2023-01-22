12532bad80e4774232a1b5391886f5df0272178f
Robin Dhillon <robindhillon1@hotmail.com>
2023-01-21 20:56:11 -0800

    added moving average func

---
+++ R/moving_average.R
@@ -0,0 +1,15 @@
+#' Calculate the moving average of a stock price for a given range of days
+#'
+#' @param stock_ticker A string corresponding to the name of the stock.
+#' @param period Number of days for which the moving average is computed
+#'
+#' @return moving_avg A tibble representing the moving average of a stock
+#' price over the specified period.
+#'
+#' @export
+#'
+#' @examples
+#' moving_average("MSFT", 20)
+moving_average <- function(stock_ticker, period){
+
+}
