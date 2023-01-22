97491e04440c45c2186adc0d1a09688d852f5455
Ruslan Dimitrov <ruslan.d.dim@gmail.com>
2023-01-21 20:41:31 -0800

    add docstrings for get_bbands, get_data, plot_bbands

---
+++ R/get_data.R
@@ -0,0 +1,16 @@
+#' Downloads all available historical daily data
+#' for stock_ticker from Yahoo finance and stores it as a
+#' csv file in data folder. If data folder does not exist
+#' it is created.
+#'
+#' @param stock_ticker string
+#' Ticker of the stock such as 'MSFT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' get_data("MSFT")
+get_data <- function(stock_ticker){
+
+}
---
+++ R/plot_bbands.R
@@ -0,0 +1,16 @@
+#' Plot stock price and corresponding upper and lower Bollinger bands
+#' for the last 200 trading days.
+#'
+#' @param stock_ticker string Ticker symbol of the
+#' stock for which the plot is created
+#'
+#' @return A line chart showing price data and corresponding upper
+#' and lower Bollinger bands.
+#'
+#' @export
+#'
+#' @examples
+#' plot_bbands("MSFT")
+plot_bbands <- function(stock_ticker){
+
+}
