deebb9b1ca7da987660130336f178a677542e68d
Ruslan Dimitrov <ruslan.d.dim@gmail.com>
2023-01-28 10:14:27 -0800

    add bbands function

--- R/get_bbands.R
+++ R/get_bbands.R
@@ -5,10 +5,23 @@
 #' @param stock_ticker String. Stock ticker of company to be analysed
 #'
 #' @return tibble containing upper and lower Bollinger bands
+#' adjusted closing price, moving average, date
 #' @export
 #'
 #' @examples
 #' get_bbands("MSFT")
 get_bbands <- function(stock_ticker){
-
+  pathfile <- paste0("../data/", stock_ticker,".csv")
+  if (file.exists(pathfile)){
+    data <- readr::read_csv(pathfile)
+  } else {
+    get_data(stock_ticker, "1986-03-13")
+    data <- readr::read_csv(pathfile)  # read stock data
+  }
+  price <- data|>
+    dplyr::select(adjusted) #get adjusted price values
+  bbands <- TTR::BBands(price)
+  bbands <- cbind(data, bbands) |>
+    dplyr::select(date,adjusted, mavg,dn, up)
+  return(bbands)
 }
