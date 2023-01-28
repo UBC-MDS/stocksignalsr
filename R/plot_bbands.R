20dcdf0a3314cd0fcdce50c896869b3ca4d5ed3d
Ruslan Dimitrov <ruslan.d.dim@gmail.com>
2023-01-28 11:33:33 -0800

    adding plot bbands

--- R/plot_bbands.R
+++ R/plot_bbands.R
@@ -13,4 +13,23 @@
 #' plot_bbands("MSFT")
 plot_bbands <- function(stock_ticker){
 
+  bbands_df <- get_bbands(stock_ticker)
+  bbands_df <- tail(bbands_df,252)
+  options(repr.plot.height = 7, repr.plot.width = 10)
+
+  bbands_plot <-
+    ggplot2::ggplot(bbands_df, ggplot2::aes(x=date)) +
+    ggplot2::geom_line(ggplot2::aes(y = up), color = "red") +
+    ggplot2::geom_line(ggplot2::aes(y = dn), color = "green") +
+    ggplot2::geom_line(ggplot2::aes(y = adjusted), color="blue", linetype="twodash") +
+    ggplot2::labs(y = "Adjusted close price", x = "Date", fill = "") +
+    ggplot2::ggtitle("Bollinger bands last 252 trading days") +
+    ggplot2::theme(plot.title = ggplot2::element_text(size = 20, face = "bold"),
+                   axis.text.x = ggplot2::element_text(size = 18, angle = 90),
+                   axis.text.y = ggplot2::element_text(size = 18, angle = 0),
+                   axis.title = ggplot2::element_text(size = 20),
+                   legend.text = ggplot2::element_text(size = 20),
+                   legend.title = ggplot2::element_text(size = 20, face = "bold"))
+  return(bbands_plot)
+
 }
